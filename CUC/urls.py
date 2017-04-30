7b08315dfc0d83492ba996a28c17825efa078bfd
Adrien Brunet <adrien@emencia.com>
2017-04-30 19:49:40 +0200

    Update CUC/urls.py to fit django1.11 standing

--- CUC/urls.py
+++ CUC/urls.py
@@ -1,11 +1,11 @@
 # coding: utf-8
 
 # DJANGO
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = patterns(
-    '',
+
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('main.urls')),
-)
+]

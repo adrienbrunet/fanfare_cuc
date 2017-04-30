82f58dc6f2f606c17e0916a4df2bc8856fb43d35
Adrien Brunet <adrien@emencia.com>
2017-04-30 12:40:13 +0200

    Update views (remove old context)

--- main/views.py
+++ main/views.py
@@ -1,23 +1,11 @@
 # coding: utf-8
 
-# PYTHON
-from datetime import datetime
 
-# DJANGO
-from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 
 class LandingPage(TemplateView):
     template_name = 'landing_page.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(LandingPage, self).get_context_data(**kwargs)
-        context['today'] = datetime.now()
-        context['concert_10_avril'] = datetime(2015, 4, 11)
-        context['LTDC15'] = datetime(2015, 9, 4)
-        context['endLTDC15'] = datetime(2015, 9, 8)
-        return context
 
 
 class TourPulignyView(TemplateView):

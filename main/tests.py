5ff2a565dfaca1af589284e55bb18fe9c0927f80
Adrien Brunet <adrien@emencia.com>
2017-04-30 19:19:09 +0200

    Adding 3 tests...

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.core.urlresolvers import reverse
+
+
+class StaticPageTest(TestCase):
+
+    def test_landing_page(self):
+        response = self.client.get('')
+        assert response.status_code == 200
+        assert 'landing_page.html' in [t.name for t in response.templates]
+
+    def test_presentation(self):
+        response = self.client.get(reverse('presentation'))
+        assert response.status_code == 200
+        assert 'presentation.html' in [t.name for t in response.templates]
+
+    def test_question(self):
+        response = self.client.get(reverse('puligny'))
+        assert response.status_code == 200
+        assert 'puligny.html' in [t.name for t in response.templates]

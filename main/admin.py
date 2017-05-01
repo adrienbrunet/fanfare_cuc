748a873f30ce75c2a7b6bafea7a9db19860a2079
Adrien Brunet <adrien@emencia.com>
2017-05-01 15:52:30 +0200

    Better widget for arrayfields in admin

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,32 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.postgres.forms import SplitArrayField
 
 from .models import QuestionPage, Track
 
 
-admin.site.register(QuestionPage)
+class QuestionPageForm(forms.ModelForm):
+    answers = SplitArrayField(
+        forms.CharField(max_length=1024, required=False),
+        10,  # number of CharField displayed
+        remove_trailing_nulls=True)
+    answers_before = SplitArrayField(
+        forms.CharField(max_length=1024, required=False),
+        10,  # number of CharField displayed
+        remove_trailing_nulls=True)
+    answers_expired = SplitArrayField(
+        forms.CharField(max_length=1024, required=False),
+        10,  # number of CharField displayed
+        remove_trailing_nulls=True)
+
+    class Meta:
+        model = QuestionPage
+        fields = "__all__"
+
+
+class QuestionPageAdmin(admin.ModelAdmin):
+    form = QuestionPageForm
+
+
+admin.site.register(QuestionPage, QuestionPageAdmin)
 admin.site.register(Track)

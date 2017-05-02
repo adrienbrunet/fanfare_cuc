7f4333cedbacd9eb8d86a83bf4297758484e1766
Adrien Brunet <adrien@emencia.com>
2017-05-03 00:13:06 +0200

    Order Track by title

--- main/models.py
+++ main/models.py
@@ -11,6 +11,9 @@ class Track(models.Model):
     title = models.CharField(max_length=255)
     midi_file = models.FileField()
 
+    class Meta:
+        ordering = ["title", ]
+
     def __str__(self):
         return self.title
 

3122abd1b656fb10c48a2c33cc99a11f6aae9689
Adrien Brunet <adrien@emencia.com>
2018-08-09 19:18:46 +0200

    :pencil2: update wording in admin

--- main/admin.py
+++ main/admin.py
@@ -9,13 +9,13 @@ from .models import Event, People, QuestionPage, Track
 
 class MyAdminSite(admin.AdminSite):
     # Text to put at the end of each page's <title>.
-    site_title = 'Fanfare CUC admin'
+    site_title = 'CUC admin'
 
     # Text to put in each page's <h1> (and above login form).
-    site_header = 'Fanfare CUC administration zone'
+    site_header = 'CUC - Administration Zone'
 
     # Text to put at the top of the admin index page.
-    index_title = 'Fanfare CUC administration'
+    index_title = 'Admin'
 
 
 admin_site = MyAdminSite()
